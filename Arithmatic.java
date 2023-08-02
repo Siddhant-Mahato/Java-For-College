048254011315d381b834e8541b34b87a0db8fe57
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-08-03 00:08:19 +0530

    Arithmatic

---
+++ Arithmatic.java
@@ -0,0 +1,15 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int p = 20, q = 10;
+        System.out.println("Result is = "+(p+q));
+        System.out.println("Result is = "+(p-q));
+        System.out.println("Result is = "+(p*q));
+        System.out.println("Result is = "+(p/q));
+        System.out.println("Result is = "+(p%q));
+
+        System.out.println("Result is - " + (p==q));
+        System.out.println("Result is - " + (p!=q));
+        System.out.println("Result is - " + (p>=q));
+        System.out.println("Result is - " + (p<=q));
+    }
+}
