f655efdcc1903ac378cbe09ab3de19f6af8c5699
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-08-04 11:13:37 +0530

    Name  User Input

---
+++ input_name.java
@@ -0,0 +1,14 @@
+// import java.util.Scanner;
+import java.util.*;
+
+
+public class input_name{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name :-");
+        String name = sc.next();
+
+        System.out.println("Name given is - " + name);
+    }
+}
