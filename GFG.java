b9f04c63050dc98db41d524db5165a58fd208604
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-11-28 08:35:55 +0530

    Intro-Program

---
+++ GFG.java
@@ -0,0 +1,14 @@
+// Importing classes from packages
+import java.io.*;
+ 
+// Main class
+public class GFG {
+ 
+    // Main driver method
+    public static void main(String[] args)
+    {
+ 
+        // Print statement
+        System.out.println("Welcome to GeeksforGeeks");
+    }
+}
