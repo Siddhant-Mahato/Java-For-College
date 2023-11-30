b7f6255142b884c39e3e70413934736e1cd6a8f7
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-11-30 11:39:00 +0530

    Java Program For Implementation Of Float And Double

---
+++ FloDou.java
@@ -0,0 +1,18 @@
+
+
+
+public class FloDou {
+
+	public static void main(String[] args)
+	{
+		// float type
+		float f = 20.25f;
+		System.out.println("f= " + f);
+
+		// double type
+		double d = 20.25;
+		System.out.println("d= " + d);
+	}
+}
+
+
