038129887657de04fbe96f4cf66fba1a11d0c233
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-02 07:07:00 +0530

    Java Program to implement Arithmetic Operators

---
+++ ArithmeticOperators.java
@@ -0,0 +1,20 @@
+// Java Program to implement Arithmetic Operators
+import java.io.*;
+
+// Drive Class
+class GFG {
+	// Main Function
+	public static void main (String[] args) {
+		
+		// Arithmetic operators
+		int a = 10;
+		int b = 3;
+	
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("a % b = " + (a % b));
+		
+	}
+}
