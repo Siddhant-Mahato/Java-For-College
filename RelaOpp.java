8c177af8d8be2b6b168146456d2ea8ac65e8d9a3
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-04 21:45:21 +0530

    Java Program to implement Relational Operators

---
+++ RelaOpp.java
@@ -0,0 +1,22 @@
+// Java Program to implement Relational Operators
+import java.io.*;
+
+// Driver Class
+class GFG {
+	// main function
+	public static void main(String[] args)
+	{
+		// Comparison operators
+		int a = 10;
+		int b = 3;
+		int c = 5;
+
+		System.out.println("a > b: " + (a > b));
+		System.out.println("a < b: " + (a < b));
+		System.out.println("a >= b: " + (a >= b));
+		System.out.println("a <= b: " + (a <= b));
+		System.out.println("a == c: " + (a == c));
+		System.out.println("a != c: " + (a != c));
+	}
+}
+
