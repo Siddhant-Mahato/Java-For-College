0444975e718a0e2b250f36cfff6bf751b545d4b3
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-05 15:40:40 +0530

    Java Program to implemenet Logical operators

---
+++ LogOp.java
@@ -0,0 +1,17 @@
+// Java Program to implemenet Logical operators
+import java.io.*;
+
+// Driver Class
+class LogOp {
+	// Main Function
+	public static void main (String[] args) {
+		// Logical operators
+		boolean x = true;
+		boolean y = false;
+	
+		System.out.println("x && y: " + (x && y));
+		System.out.println("x || y: " + (x || y));
+		System.out.println("!x: " + (!x));
+	}
+}
+
