f9c2ffb75a73715c2fee9006bdaa4b947da43615
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-06 12:01:31 +0530

    Java program to illustrate max of three numbers using ternary operator.

---
+++ OpeTer.java
@@ -0,0 +1,21 @@
+
+
+
+// Java program to illustrate max of three numbers using ternary operator.
+public class operators {
+	public static void main(String[] args)
+	{
+		int a = 20, b = 10, c = 30, result;
+
+		// result holds max of three
+		// numbers
+		result
+			= ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+		System.out.println("Max of three numbers = "
+						+ result);
+	}
+}
+
+
+
+
