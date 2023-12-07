4e1f7a7e76679d95fc443304696de737fe5eb2fb
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-08 00:24:34 +0530

    Java Program to implement shift operators

---
+++ ShOp.java
@@ -0,0 +1,18 @@
+// Java Program to implement shift operators
+import java.io.*;
+
+// Driver Class
+class GFG {
+	// main function
+	public static void main(String[] args)
+	{
+		int a = 10;
+	
+		// using left shift
+		System.out.println("a<<1 : " + (a << 1));
+	
+		// using right shift
+		System.out.println("a>>1 : " + (a >> 1));
+	}
+}
+
