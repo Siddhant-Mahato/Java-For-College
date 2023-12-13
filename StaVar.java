7c1ada8cba3daaeeb7cb360b8e44baf056d65db2
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-13 14:21:22 +0530

    Java Program to demonstrate Static variables

---
+++ StaVar.java
@@ -0,0 +1,22 @@
+// Java Program to demonstrate Static variables
+import java.io.*;
+
+class GFG {
+	// Declared static variable
+	public static String geek = "Shubham Jain";
+
+	public static void main(String[] args)
+	{
+
+		// geek variable can be accessed without object
+		// creation Displaying O/P GFG.geek --> using the
+		// static variable
+		System.out.println("Geek Name is : " + GFG.geek);
+
+		// static int c=0;
+		// above line,when uncommented,
+		// will throw an error as static variables cannot be
+		// declared locally.
+	}
+}
+
