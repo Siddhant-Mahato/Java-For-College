7ccad82573ca49c1dbbbbcd1b4fcd7f4ab5e389d
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-12-16 16:45:42 +0530

    Update Arithmatic.java

--- Arithmatic.java
+++ Arithmatic.java
@@ -1,4 +1,6 @@
+
 public class Arithmatic {
+    
     public static void main(String[] args) {
         int p = 20, q = 10;
         System.out.println("Result is = "+(p+q));
@@ -12,4 +14,5 @@ public class Arithmatic {
         System.out.println("Result is - " + (p>=q));
         System.out.println("Result is - " + (p<=q));
     }
+    
 }
